226adcf0e36ca44999c6f22c3d9abcf96efa7b49
Monica Gerard <mlgerardvla@gmail.com>
2020-03-12 22:47:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []#
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    index = 0
+    customer_list = ""
+    katz_deli.each do |customer|
+      customer_list << " #{index + 1}. #{customer}"
+      index += 1
+    end
+    puts "The line is currently:#{customer_list}"
+  end
+end
