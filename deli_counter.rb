c9ff396eb0125f16fc6d48c8a6923a224a7aaa49
Monica Gerard <mlgerardvla@gmail.com>
2020-03-12 23:01:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,12 @@ def take_a_number(katz_deli, customer)
     puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
     return katz_deli
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"    
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift  
+  end
+end
