8624a1ce4d990dfca8250de49b5e7c019201e368
Monica Gerard <mlgerardvla@gmail.com>
2020-03-12 22:56:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ end
 
 def take_a_number(katz_deli, customer)
     katz_deli.push(customer)
-    puts "Welcome, #{customer}. You are number #{katz_deli.length + 1} in line."
+    puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
     return katz_deli
 end
